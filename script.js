f38f7da69d21a0ccc0b50979b83408d48c166add
Krusto69 <adamheathmt@gmail.com>
2025-08-19 16:22:05 +0100

    modified the js file to reflect the removal of the container

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
 
 function createGrid() {
-    let container = document.querySelector(".container");
+    let body = document.querySelector("body");
     let grid = document.createElement("div");
     grid.classList.add("grid");
-    for(let i=0;i<16;i++){
+    for(let i=0;i<200;i++){
         grid.appendChild(createRow())
     }
-    container.appendChild(grid);
+    body.appendChild(grid);
 }
 
 function createRow(){
     let row = document.createElement("div");
     row.classList.add("row");
-    for(let i=0;i<16;i++){
+    for(let i=0;i<200;i++){
         row.appendChild(createBlock());
     }
     return row
